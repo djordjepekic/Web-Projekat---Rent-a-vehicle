95eb943e1480ac5e61c3d64337acff89e90f7794
odbeast <stefan.stojakovic@gmail.com>
2018-06-05 12:10:18 +0200

    RADBContext DbSets updated

--- RentApp/Persistance/RADBContext.cs
+++ RentApp/Persistance/RADBContext.cs
@@ -10,10 +10,14 @@ namespace RentApp.Persistance
 {
     public class RADBContext : IdentityDbContext<RAIdentityUser>
     {
+        public virtual DbSet<AppUser> AppUsers { get; set; }
         public virtual DbSet<Service> Services { get; set; }
         public virtual DbSet<Office> Offices { get; set; }
         public virtual DbSet<Vehicle> Vehicles { get; set; }
-        public virtual DbSet<AppUser> AppUsers { get; set; }
+        public virtual DbSet<VehicleType> VehicleTypes { get; set; }
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<PriceList> PriceLists { get; set; }
+        public virtual DbSet<PriceListItem> PriceListItems { get; set; }
 
         public RADBContext() : base("name=RADB")
         {           
