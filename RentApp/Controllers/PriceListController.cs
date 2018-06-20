176508a65a3906cbdaef5c600f6c1a542a717976
odbeast <stefan.stojakovic@gmail.com>
2018-06-20 15:59:04 +0200

    PriceListController added

---
+++ RentApp/Controllers/PriceListController.cs
@@ -0,0 +1,22 @@
+﻿using RentApp.Models.Entities;
+using RentApp.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RentApp.Controllers
+{
+    [RoutePrefix("api/PriceList")]
+    public class PriceListController : ApiController
+    {
+        RADBContext db = new RADBContext();
+
+        public IQueryable<PriceListItem> GetAllPriceListItems()
+        {
+            return db.PriceListItems;
+        }
+    }
+}
